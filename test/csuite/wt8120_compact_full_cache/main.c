a03a090102fd4a028f4c93868b2cb5110664c167
Alexey Anisimov <alexey.anisimov@mongodb.com>
2021-09-27 17:44:52 +1000

    Intermediate commit.

--- test/csuite/wt8120_compact_full_cache/main.c
+++ test/csuite/wt8120_compact_full_cache/main.c
@@ -28,17 +28,29 @@
 #include "test_util.h"
 
 /*
- * 
+ *
  */
 
-#define NUM_RECORDS 1000000
+/* If you modify number of records or data content, make sure to update DATA_SIZE accordingly. */
+#define NUM_RECORDS 3000000
+
+/*
+ * This is WiredTigerCheckpoint size in MB from meatadata for
+ * "file:test_wt8120_compact_full_cache.wt".
+ */
+#define DATA_SIZE 2629
+
+/* Approximate file size in MB without compact. */
+#define FILE_SIZE_NO_COMPACT 5250
 
 /* Constants and variables declaration. */
 /*
  * You may want to add "verbose=[compact,compact_progress]" to the connection config string to get
  * better view on what is happening.
  */
-static const char conn_config[] = "create,cache_size=2GB,statistics=(all)";
+static const char conn_config[] =
+  "create,cache_size=1GB,statistics=(all),statistics_log=(wait=1,json),verbose=[compact,compact_"
+  "progress]";
 static const char table_config[] =
   "allocation_size=4KB,leaf_page_max=4KB,key_format=i,value_format=QQQS";
 static char data_str[1024] = "";
@@ -47,39 +59,57 @@ static char data_str[1024] = "";
 struct thread_data {
     WT_CONNECTION *conn;
     const char *uri;
+    int begin, end;
 };
 
 /* Forward declarations. */
 static void run_test(const char *home, const char *uri);
+static void *thread_func_update(void *arg);
 static void *thread_func_compact(void *arg);
 static void populate(WT_SESSION *session, const char *uri);
 static void remove_records(WT_SESSION *session, const char *uri);
+static void update_records(WT_SESSION *session, const char *uri, int begin, int end);
 static uint64_t get_file_size(WT_SESSION *session, const char *uri);
 
 /* Methods implementation. */
 int
 main(int argc, char *argv[])
 {
-    TEST_OPTS opts;
+    TEST_OPTS *opts, _opts;
 
-    memset(&opts, 0, sizeof(opts));
-    testutil_check(testutil_parse_opts(argc, argv, &opts));
+    opts = &_opts;
+    memset(opts, 0, sizeof(*opts));
+    testutil_check(testutil_parse_opts(argc, argv, opts));
 
-    run_test(true, opts.home, opts.uri);
+    run_test(opts->home, opts->uri);
 
-    testutil_cleanup(&opts);
+    testutil_cleanup(opts);
 
     return (EXIT_SUCCESS);
 }
 
+#define THREADS_NUM 10
+
 static void
 run_test(const char *home, const char *uri)
 {
-    struct thread_data td;
     WT_CONNECTION *conn;
     WT_SESSION *session;
-    pthread_t thread_compact;
-    uint64_t file_sz_after, file_sz_before;
+    pthread_t thread_compact, threads_update[THREADS_NUM];
+    uint64_t file_sz_after;
+    int i;
+    struct thread_data td[THREADS_NUM] = {
+      {NULL, uri, 1, NUM_RECORDS / 3},
+      {NULL, uri, (NUM_RECORDS * 2) / 3, NUM_RECORDS},
+      {NULL, uri, 1, NUM_RECORDS / 3},
+      {NULL, uri, (NUM_RECORDS * 2) / 3, NUM_RECORDS},
+      {NULL, uri, 1, NUM_RECORDS / 3},
+      {NULL, uri, (NUM_RECORDS * 2) / 3, NUM_RECORDS},
+      {NULL, uri, 1, NUM_RECORDS / 3},
+      {NULL, uri, (NUM_RECORDS * 2) / 3, NUM_RECORDS},
+      {NULL, uri, 1, NUM_RECORDS / 3},
+      {NULL, uri, (NUM_RECORDS * 2) / 3, NUM_RECORDS},
+    };
 
     testutil_make_work_dir(home);
     testutil_check(wiredtiger_open(home, NULL, conn_config, &conn));
@@ -89,33 +119,71 @@ run_test(const char *home, const char *uri)
     /* Create and populate table. Checkpoint the data after that. */
     testutil_check(session->create(session, uri, table_config));
 
+    printf("Populating table...\n");
     populate(session, uri);
     testutil_check(session->checkpoint(session, NULL));
 
     /*
      * Remove 1/3 of data from the middle of the key range to let compact relocate blocks from the
-     * end of the file.
+     * end of the file. Checkpoint the changes after the removal.
      */
+    printf("Removing records...\n");
     remove_records(session, uri);
+    testutil_check(session->checkpoint(session, NULL));
 
-    file_sz_before = get_file_size(session, uri);
+    for (i = 0; i < THREADS_NUM; i++) {
+        td[i].conn = conn;
+        testutil_check(pthread_create(&threads_update[i], NULL, thread_func_update, &td[i]));
+    }
 
-    /* Spawn checkpoint and compact threads. Order is important! */
-    testutil_check(pthread_create(&thread_compact, NULL, thread_func_compact, &td));
+    testutil_check(pthread_create(&thread_compact, NULL, thread_func_compact, &td[0]));
 
     /* Wait for the threads to finish the work. */
+    for (i = 0; i < THREADS_NUM; i++)
+        (void)pthread_join(threads_update[i], NULL);
+
     (void)pthread_join(thread_compact, NULL);
 
+    testutil_check(session->checkpoint(session, NULL));
     file_sz_after = get_file_size(session, uri);
 
+    /* Wait for stat */
+    __wt_sleep(2, 0);
+
     testutil_check(session->close(session, NULL));
+    session = NULL;
+
+    testutil_check(conn->close(conn, NULL));
+    conn = NULL;
 
     /* Check if there's at least 10% compaction. */
-    printf(" - Compressed file size MB: %f\n - Original file size MB: %f\n",
-      file_sz_after / (1024.0 * 1024), file_sz_before / (1024.0 * 1024));
+    printf(
+      " - Compressed file size is: %f MB\n - Approximate data size from metedata is: %d MB\n - "
+      "Approximate file size without compact: %d MB\n",
+      file_sz_after / (1024.0 * 1024), DATA_SIZE, FILE_SIZE_NO_COMPACT);
 
     /* Make sure the compact operation has reduced the file size by at least 20%. */
-    testutil_assert((file_sz_before / 100) * 80 > file_sz_after);
+    // testutil_assert((file_sz_before / 100) * 80 > file_sz_after);
+}
+
+static void *
+thread_func_update(void *arg)
+{
+    struct thread_data *td;
+    WT_SESSION *session;
+
+    td = (struct thread_data *)arg;
+
+    testutil_check(td->conn->open_session(td->conn, NULL, NULL, &session));
+
+    printf("Updating records...\n");
+    update_records(session, td->uri, td->begin, td->end);
+    printf("Updating finished.\n");
+
+    testutil_check(session->close(session, NULL));
+    session = NULL;
+
+    return (NULL);
 }
 
 static void *
@@ -128,36 +196,15 @@ thread_func_compact(void *arg)
 
     testutil_check(td->conn->open_session(td->conn, NULL, NULL, &session));
 
-    if (td->cond != NULL) {
-        /*
-         * Make sure checkpoint thread is initialized and waiting for the signal. Sleep for one
-         * second.
-         */
-        __wt_sleep(1, 0);
-
-        /* Wake up the checkpoint thread. */
-        printf("Sending the signal!\n");
-        __wt_cond_signal((WT_SESSION_IMPL *)session, td->cond);
-    }
-
     /* Perform compact operation. */
+    printf("Compacting table...\n");
     testutil_check(session->compact(session, td->uri, NULL));
+    printf("Compacting finished.\n");
 
     testutil_check(session->close(session, NULL));
+    session = NULL;
 
     return (NULL);
-}
-
-static bool
-wait_run_check(WT_SESSION_IMPL *session)
-{
-    (void)session; /* Unused */
-
-    /*
-     * Always return true to make sure __wt_cond_wait_signal does wait. This callback is required
-     * with waits longer that one second.
-     */
-    return (true);
 }
 
 static void
@@ -185,6 +232,7 @@ populate(WT_SESSION *session, const char *uri)
     }
 
     testutil_check(cursor->close(cursor));
+    cursor = NULL;
 }
 
 static void
@@ -202,6 +250,29 @@ remove_records(WT_SESSION *session, const char *uri)
     }
 
     testutil_check(cursor->close(cursor));
+    cursor = NULL;
+}
+
+static void
+update_records(WT_SESSION *session, const char *uri, int begin, int end)
+{
+    WT_CURSOR *cursor;
+    size_t buf_size;
+    int i;
+
+    testutil_check(session->open_cursor(session, uri, NULL, NULL, &cursor));
+
+    buf_size = sizeof(data_str) / sizeof(data_str[0]);
+    memset(data_str, 'a', buf_size - 1);
+    data_str[buf_size - 1] = '\0';
+    for (i = begin; i < end; i++) {
+        cursor->set_key(cursor, i);
+        cursor->set_value(cursor, 1, 2, 3, data_str);
+        testutil_check(cursor->update(cursor));
+    }
+
+    testutil_check(cursor->close(cursor));
+    cursor = NULL;
 }
 
 static uint64_t
@@ -217,6 +288,7 @@ get_file_size(WT_SESSION *session, const char *uri)
     testutil_check(cur_stat->search(cur_stat));
     testutil_check(cur_stat->get_value(cur_stat, &descr, &str_val, &val));
     testutil_check(cur_stat->close(cur_stat));
+    cur_stat = NULL;
 
     return (val);
 }
