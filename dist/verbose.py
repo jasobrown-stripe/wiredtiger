a215e38813833c71f9defddea832c62497bd585d
Etienne Petrel <etienne.petrel@mongodb.com>
2021-12-01 11:30:38 +0000

    Add more checks to the script

--- dist/verbose.py
+++ dist/verbose.py
@@ -3,66 +3,84 @@
 import re
 from dist import compare_srcfile
 
-filename = "../src/include/wiredtiger.in"
 verbose_categories = []
+filename = '../src/include/wiredtiger.in'
+start_tag = 'VERBOSE ENUM START'
+stop_tag = 'VERBOSE ENUM STOP'
 
-# Retrieve all verbose categories
+# Retrieve all verbose categories.
 with open(filename, 'r') as f:
     in_section = False
     pattern = re.compile("^WT_VERB_[A-Z_]+$")
     for line in f:
-        if line.find('VERBOSE ENUM START') != -1:
-            # The next line is in the section
+        if line.find(start_tag) != -1:
             in_section = True
             continue
-        if line.find('VERBOSE ENUM STOP') != -1:
+        if line.find(stop_tag) != -1:
             break
         if in_section:
-            # Remove any leading and trailing whitespaces 
+            # Remove any leading and trailing whitespaces.
             line = line.strip()
             content = line.split(',')
 
-            # Sanity checks
-            assert(len(content) > 1)
+            # The length will be <= 1 if no comma is present.
+            assert len(content) > 1, content
             verbose_category = content[0]
-            assert pattern.match(verbose_category), "The category "  + verbose_category + " does not follow the expected syntax."
+            # Check the value follows the expected format.
+            assert pattern.match(verbose_category), \
+                   'The category '  + verbose_category + ' does not follow the expected syntax.'
 
-            # Save the category
+            # Save the category.
             verbose_categories.append(content[0])
 
-    assert in_section, "No verbose categories have been found in " + filename
+    assert len(verbose_categories) > 0, 'No verbose categories have been found in ' + filename
 f.close()
 
-filename = "../src/include/verbose.h"
+filename = '../src/include/verbose.h'
+start_tag = 'AUTOMATIC VERBOSE ENUM STRING GENERATION START'
+stop_tag = 'AUTOMATIC VERBOSE ENUM STRING GENERATION STOP'
 
 # Generate all verbose categories as strings
 tmp_file = '__tmp_verbose'
 with open(filename, 'r') as f:
 
     tfile = open(tmp_file, 'w')
+    end_found = False
     in_section = False
+    start_found = False
 
     for line in f:
 
         line_tmp = line
 
-        if line.find('AUTOMATIC VERBOSE ENUM STRING GENERATION STOP') != -1:
+        if line.find(stop_tag) != -1:
+            assert start_found, 'Missing start tag: ' + start_tag
+            assert not end_found, 'Found end tag again: ' + stop_tag
+
+            end_found = True
             in_section = False
-        elif line.find('AUTOMATIC VERBOSE ENUM STRING GENERATION START') != -1:
+
+        elif line.find(start_tag) != -1:
+            assert not start_found and not end_found, 'Duplicate tag'
+
+            start_found = True
+            in_section = True
+
             escape_char = ''
             if line.strip().endswith('\\'):
                 escape_char = '\\'
+
             indentation = len(line) - len(line.lstrip())
-            # The next line is in the section
-            in_section = True
+
             for category in verbose_categories:
                 line_tmp += indentation * ' ' + "\"" + category + "\", %s\n" % escape_char
+
         elif in_section:
             continue
 
         tfile.write(line_tmp)
 
-    assert not in_section, "Error"
+    assert not in_section and start_found and end_found, 'File syntax incorrect'
 
     tfile.close()
     compare_srcfile(tmp_file, filename)
